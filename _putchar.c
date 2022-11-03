4cf091a5256ea3717ac783df2fbb6a6fb53cdcb1
MicoBledsoe <MicoMods@gmail.com>
2022-11-03 13:31:45 -0700

    Function

---
+++ _putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - is a function that writes the character c to the stdout.
+ * @c: character to print
+ *
+ * Return: 1 is returned on success, -1 is given on error.
+*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
